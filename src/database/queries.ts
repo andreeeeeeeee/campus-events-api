f39773fc6c88d0b2f09df16be3ec48d68d7be406
André Bastilhos <abastilhossouza@gmail.com>
2026-09-15 21:50:32 -0300

    FEAT: Criar POST /events (#2)

--- src/database/queries.ts
+++ src/database/queries.ts
@@ -1,11 +1,12 @@
 import { HttpError } from '../errors';
 import { getPool } from './pool';
-import { Person } from './types';
+import { Event, EventInput, Person } from './types';
 
 export interface Queries {
   checkConnection(): Promise<boolean>;
   getAllPeople(): Promise<Person[]>;
   addPerson(person: Person): Promise<Person>;
+  addEvent(event: EventInput): Promise<Event>;
 }
 
 export const makeQueries = (databaseUrl: string): Queries => {
@@ -37,6 +38,20 @@ export const makeQueries = (databaseUrl: string): Queries => {
         RETURNING name, age
         `,
         [name, age],
+      );
+      if (rowCount !== 1) {
+        throw new HttpError(500, 'Something went wrong');
+      }
+      return rows[0];
+    },
+    addEvent: async ({ title, starts_at, ends_at, room, organizer }) => {
+      const { rows, rowCount } = await pool.query<Event, [string, Date, Date, string, string]>(
+        `
+        INSERT INTO events (title, starts_at, ends_at, room, organizer)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING id, title, starts_at, ends_at, room, organizer, created_at
+        `,
+        [title, starts_at, ends_at, room, organizer],
       );
       if (rowCount !== 1) {
         throw new HttpError(500, 'Something went wrong');
